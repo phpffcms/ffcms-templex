9ec505df6d9768e0b91f1006ab7f388997fcd38a
zenn <zenn1989@gmail.com>
2018-05-13 19:36:54 +0300

    Template render fallback dir

--- src/Engine.php
+++ src/Engine.php
@@ -2,6 +2,7 @@
 
 namespace Ffcms\Templex;
 
+use Ffcms\Core\Helper\FileSystem\Directory;
 use Ffcms\Templex\Helper\Html\Bootstrap4;
 use Ffcms\Templex\Helper\Html\Form;
 use Ffcms\Templex\Helper\Html\Listing;
@@ -37,5 +38,29 @@ class Engine extends \League\Plates\Engine
             new Form(),
             new Bootstrap4()
         ]);
+    }
+
+    /**
+     * Create a new template and render it.
+     * @param string $name
+     * @param array $data
+     * @param null $fallbackDir
+     * @return string
+     */
+    public function render($name, array $data = array(), $fallbackDir = null)
+    {
+        try {
+            $render = parent::render($name, $data);
+        } catch (\Exception $e) {
+            if ($fallbackDir && Directory::exist($fallbackDir)) {
+                // try to use fallback directory if exception catched
+                $currentDir = parent::getDirectory();
+                parent::setDirectory($fallbackDir);
+                $render = parent::render($name, $data);
+                parent::setDirectory($currentDir);
+            }
+        }
+
+        return $render;
     }
 }
